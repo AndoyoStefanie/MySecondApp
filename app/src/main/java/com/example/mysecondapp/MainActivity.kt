93ca36c1f3c26da9dfa42a1466ae85d0f9382690
AndoyoStefanie <89018831+AndoyoStefanie@users.noreply.github.com>
2022-08-23 08:44:46 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/mysecondapp/MainActivity.kt
+++ app/src/main/java/com/example/mysecondapp/MainActivity.kt
@@ -1,15 +1,13 @@
 package com.example.mysecondapp
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
-import com.example.mysecondapp.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.MySecondApp.R
+import com.example.MySecondApp.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,10 +26,17 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
+        val onClickListener = binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+    }
+
+    private fun setupActionBarWithNavController(
+        navController: Any,
+        appBarConfiguration: AppBarConfiguration
+    ) {
+        TODO("Not yet implemented")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -55,4 +60,10 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
+
+    private fun findNavController(navHostFragmentContentMain: Int): Any {
+    }
+}
+private fun Any.navigateUp(appBarConfiguration: AppBarConfiguration): Boolean {
+
 }
