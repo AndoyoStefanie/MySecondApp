6cf1f8c616dcb061ea5a4614da61db70cf5221ea
AndoyoStefanie <89018831+AndoyoStefanie@users.noreply.github.com>
2022-08-23 08:50:08 +0800

    Update SecondFragment.kt

--- app/src/main/java/com/example/mysecondapp/SecondFragment.kt
+++ app/src/main/java/com/example/mysecondapp/SecondFragment.kt
@@ -1,12 +1,12 @@
-package com.example.mysecondapp
+package com.example.MySecondApp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.mysecondapp.databinding.FragmentSecondBinding
+import com.example.MySecondApp.databinding.FragmentSecondBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
